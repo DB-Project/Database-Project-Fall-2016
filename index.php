30ba931c63a4f952b7c786c7927e5fcc0d1974d1
hskim105 <hk1798@nyu.edu>
2016-12-02 19:54:12 -0500

    Created login button in index.php and login.php has the forum

--- index.php
+++ index.php
@@ -7,12 +7,6 @@
 	</head>
 
 	<body>
-			<form action="login.php" method="POST">
-				<p>Username:</p><input type="text" name="user">
-				<p>Password:</p><input type="password" name="pass">
-				<br />
-				<input type="submit" name="login" />
-			</form>
-
+			<a href="login.php">Login</a>
 	</body>
 </html>
---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Login Page
+		</title>
+	</head>
+
+	<body>
+      <h1>Please login here</h1>
+			<form action="asdf.php" method="POST">
+				<p>Username:</p><input type="text" name="user">
+				<p>Password:</p><input type="password" name="pass">
+				<br />
+				<input type="submit" name="login" />
+			</form>
+
+	</body>
+</html>
