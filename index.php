ccbed29288e9a955b5062ba35487534e0f946028
[Adnan] <am6377@nyu.edu>
2016-12-02 19:22:10 -0500

    I am adding the login.php file and changed the name of the orginal php file to index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Find Friends
+		</title>
+	</head>
+
+	<body>
+			<form action="login.php" method="POST">
+				<p>Username:</p><input type="text" name="user">
+				<p>Password:</p><input type="password" name="pass">
+				<br />
+				<input type="submit" name="login" />
+			</form>
+
+	</body>
+</html>
