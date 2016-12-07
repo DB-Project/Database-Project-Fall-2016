95df9ae40b3b375b6d6b1e6779591e9440f80559
Victor Kim <hskim105@users.noreply.github.com>
2016-12-07 17:00:16 -0500

    Manually fixing merge conflicts

--- index.php
+++ index.php
@@ -8,10 +8,6 @@
 
 	<body>
 		<a href="signup.php">Sign Up</a>
-		<a href="login.php">Login</a>
-<<<<<<< HEAD
-	</body>
-=======
+		<a href="login.php">Login</a>=
 	</body>
 </html>
->>>>>>> a5b1a7fffb9eca20f91bb12309107710ca1493cc
