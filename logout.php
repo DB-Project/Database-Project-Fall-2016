61da0f01a2a2adeb9ada578dad8c3b01a241c60c
Victor Kim <hk1798@nyu.edu>
2016-12-07 23:54:21 -0500

    Timed auto redirect for logout.php

--- logout.php
+++ logout.php
@@ -2,5 +2,11 @@
 	session_start();
 	unset($_SESSION['session_user']);
 	session_destroy();
-	header("Location: index.php")
+	
+	//Direct logout. No time
+	//header("Location: index.php");
+	
+	//For timed redirect
+	echo "You are logged out. You will be redired in 5 seconds";
+	header("Refresh: 5; index.php");
 ?>
