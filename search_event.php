f87ff23e07faf9e648c3ae6dbdd2be78ef096bb8
Victor Kim <hk1798@nyu.edu>
2016-12-11 06:23:11 -0500

    Successfully implemented search event with same interest as user

--- search_event.php
+++ search_event.php
@@ -4,34 +4,36 @@
 ?>
 
 <?php
-	function sharedGroupInterest($userName, $DB_LINK){
-		$theQuery = "SELECT group_id FROM about, interested_in WHERE username = '$userName' AND about.keyword = interested_in.keyword AND about.category = interested_in.category";
-		$theResult = mysqli_query($DB_LINK, $theQuery);
-		if($theResult){
-			//Assuming the user inputted the correct data
-			$resultArray = array();
-			while($theRow = mysqli_fetch_row($theResult)){
-				$resultArray[] = $theRow;
-			}
-			return $resultArray;
-		else{
-			//ERROR CHECKING
-			error_reporting(E_ALL | E_WARNING | E_NOTICE);
-			ini_set('display_errors', TRUE);
-
-			echo "SQL Error: " . mysqli_error($DB_LINK);
-		}
-	}
+	//TODO: THIS CODE HAS ERROR
+	// function sharedGroupInterest($userName, $DB_LINK){
+// 		$theQuery = "SELECT group_id FROM about, interested_in WHERE username = '$userName' AND about.keyword = interested_in.keyword AND about.category = interested_in.category";
+// 		$theResult = mysqli_query($DB_LINK, $theQuery);
+// 		if($theResult){
+// 			//Assuming the user inputted the correct data
+// 			$resultArray = array();
+// 			while($theRow = mysqli_fetch_row($theResult)){
+// 				$resultArray[] = $theRow;
+// 			}
+// 			return $resultArray;
+// 		else{
+// 			//ERROR CHECKING
+// 			error_reporting(E_ALL | E_WARNING | E_NOTICE);
+// 			ini_set('display_errors', TRUE);
+//
+// 			echo "SQL Error: " . mysqli_error($DB_LINK);
+// 		}
+// 	}
 
 	function searchSharedInterest($userName, $DB_LINK){
 		$theQuery = "SELECT * FROM an_event NATURAL JOIN organize WHERE group_id IN (SELECT group_id FROM about, interested_in WHERE username = '$userName' AND about.keyword = interested_in.keyword AND about.category = interested_in.category)";
 
 		$theResult = mysqli_query($DB_LINK, $theQuery);
-		$resultArray = "";
-		while($theRow = mysqli_fetch_array($resultLocation)){
-			echo $theRow;
+
+		while($theRow = mysqli_fetch_array($theResult)){
+			echo '<pre>';
+			print_r ($theRow);
+			echo '</pre>';
 		}
-		return $resultArray;
 	}
 ?>
 
@@ -39,15 +41,18 @@
 <html>
 	<head>
 		<title>
-			Friends
+			Events of Shared Interest
 		</title>
 	</head>
 	<body>
+		<a href="homepage.php">Home</a>
+		<a href="logout.php">Logout</a>
+		
 		<?php
 			include("db_connection.php");
-			echo "Hello";
-			//TODO: FIX ME. IMPLEMENTATION NEEDED
-			// searchSharedInterest($_SESSION['session_user'], $DB_LINK);
+			echo "<br> The following events share the same interest as you, " . $_SESSION['session_user'];
+			//TODO: Fix format
+			searchSharedInterest($_SESSION['session_user'], $DB_LINK);
 		?>
 		
 	</body>
