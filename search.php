7ae10e72d29d2d40e066979a9ab7f3fb3bf2baa8
Victor Kim <hk1798@nyu.edu>
2016-12-11 16:46:48 -0500

    cleaning serach class

--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 <?php
 	class Search{
-		
+		//Prints the result of the query
 		public static function printResults($theResult){
 			while($theRow = mysqli_fetch_array($theResult)){
 				echo '<pre>';
@@ -9,6 +9,7 @@
 			}
 		}
 		
+		//Search friends' events
 		public static function searchFriendEvent($userID, $DB_LINK){
 			$theQuery = "SELECT DISTINCT title, description, start_time, end_time, location_name, zipcode FROM sign_up NATURAL JOIN an_event WHERE username IN (SELECT friend_to FROM friend WHERE friend_of = '$userID')";
 			
@@ -21,5 +22,21 @@
 				echo "No results";
 			}
 		}
+		
+		public static function searchSharedInterest($userName, $DB_LINK){
+			$theQuery = "SELECT * FROM an_event NATURAL JOIN organize WHERE group_id IN (SELECT group_id FROM about, interested_in WHERE username = '$userName' AND about.keyword = interested_in.keyword AND about.category = interested_in.category)";
+
+			$theResult = mysqli_query($DB_LINK, $theQuery);
+
+			if($theResult){
+				self::printResults($theResult);
+			}
+			else{
+				echo "No results";
+			}
+		}
+		
+		
+		
 	}
 ?>
--- search_event.php
+++ search_event.php
@@ -2,41 +2,6 @@
 	//Resumes session that was created in login
 	session_start();
 ?>
-
-<?php
-	//TODO: THIS CODE HAS ERROR
-	// function sharedGroupInterest($userName, $DB_LINK){
-// 		$theQuery = "SELECT group_id FROM about, interested_in WHERE username = '$userName' AND about.keyword = interested_in.keyword AND about.category = interested_in.category";
-// 		$theResult = mysqli_query($DB_LINK, $theQuery);
-// 		if($theResult){
-// 			//Assuming the user inputted the correct data
-// 			$resultArray = array();
-// 			while($theRow = mysqli_fetch_row($theResult)){
-// 				$resultArray[] = $theRow;
-// 			}
-// 			return $resultArray;
-// 		else{
-// 			//ERROR CHECKING
-// 			error_reporting(E_ALL | E_WARNING | E_NOTICE);
-// 			ini_set('display_errors', TRUE);
-//
-// 			echo "SQL Error: " . mysqli_error($DB_LINK);
-// 		}
-// 	}
-
-	function searchSharedInterest($userName, $DB_LINK){
-		$theQuery = "SELECT * FROM an_event NATURAL JOIN organize WHERE group_id IN (SELECT group_id FROM about, interested_in WHERE username = '$userName' AND about.keyword = interested_in.keyword AND about.category = interested_in.category)";
-
-		$theResult = mysqli_query($DB_LINK, $theQuery);
-
-		while($theRow = mysqli_fetch_array($theResult)){
-			echo '<pre>';
-			print_r ($theRow);
-			echo '</pre>';
-		}
-	}
-?>
-
 <!DOCTYPE html>
 <html>
 	<head>
@@ -50,9 +15,10 @@
 		
 		<?php
 			include("db_connection.php");
+			include("search.php");
 			echo "<br> The following events share the same interest as you, " . $_SESSION['session_user'];
-			//TODO: Fix format
-			searchSharedInterest($_SESSION['session_user'], $DB_LINK);
+			
+			Search::searchSharedInterest($_SESSION['session_user'], $DB_LINK);
 		?>
 		
 	</body>
