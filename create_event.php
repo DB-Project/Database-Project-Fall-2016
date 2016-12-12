76f0d47840b8da92b1cdb8c08ebeea3bd7d7bc81
[Adnan] <am6377@nyu.edu>
2016-12-11 20:26:28 -0500

    Added drop down for entering start and end time with YYYY-MM-DDTHH:MM:SSZ format

--- create_event.php
+++ create_event.php
@@ -37,8 +37,184 @@
 			
 <!--TODO: Put constraint on time so that start time cannot be after end time -->
 			
-			<p>Start Time (Format: YYYY-MM-DDTHH:MM:SSZ)</p><input type="datetime" name="startTime">
-			<p>End Time (Format: YYYY-MM-DDTHH:MM:SSZ)</p><input type="datetime" name="endTime">
+			<p>Start Time (Format: YYYY-MM-DDTHH:MM:SSZ)</p>
+
+			<select name = "YYYY">
+			  <option value="2015">2015</option>
+			  <option value="2016">2016</option>
+			  <option value="2017">2017</option>
+			  <option value="2018">2018</option>
+			  <option value="2019">2019</option>
+			</select>
+			<select name = "Month">
+			  <option value="01">01</option>
+			  <option value="02">02</option>
+			  <option value="03">03</option>
+			  <option value="04">04</option>
+			  <option value="05">05</option>
+			  <option value="06">06</option>
+			  <option value="07">07</option>
+			  <option value="08">08</option>
+			  <option value="09">09</option>
+			  <option value="10">10</option>
+			  <option value="11">11</option>
+			  <option value="12">12</option>
+			 </select>
+			
+
+			 <select name = "DD">
+			 	<?php  
+					for ($x = 0; $x <= 9; $x++) {
+  						echo "<option value=0".$x. "> 0" .$x. "</option>";
+					}
+					for ($x = 10; $x <= 31; $x++) {
+  						echo "<option value=".$x. ">" .$x. "</option>";
+					}
+				?> 
+			 </select>
+
+			 <select name = "Tval">
+			 	<option value="T">T</option>
+			 </select>
+
+
+			 <select name = "HH">
+			 	<?php  
+					for ($x = 0; $x <= 9; $x++) {
+  						echo "<option value=0".$x. "> 0" .$x. "</option>";
+					}
+					for ($x = 10; $x <= 12; $x++) {
+  						echo "<option value=".$x. ">" .$x. "</option>";
+					}
+				?> 
+			 </select>
+
+			 <select name = "MM">
+			 	<?php  
+					for ($x = 0; $x <= 9; $x++) {
+  						echo "<option value=0".$x. "> 0" .$x. "</option>";
+					}
+					for ($x = 10; $x <= 60; $x++) {
+  						echo "<option value=".$x. ">" .$x. "</option>";
+					}
+				?> 
+			 </select>
+
+			 <select name = "SS">
+			 	<?php  
+					for ($x = 0; $x <= 9; $x++) {
+  						echo "<option value=0".$x. "> 0" .$x. "</option>";
+					}
+					for ($x = 10; $x <= 60; $x++) {
+  						echo "<option value=".$x. ">" .$x. "</option>";
+					}
+
+				?> 
+			 </select>
+
+			 <select name = "Zval">
+			 	<option value="Z">T</option>
+			 </select>
+			 <!--  -->
+			 <!--  -->
+			 <!--  END OF START TIME -->
+			 <!--  -->
+			 <!--  -->
+			 <p>End Time (Format: YYYY-MM-DDTHH:MM:SSZ)</p>
+
+			<select name = "YYYYe">
+			  <option value="2015">2015</option>
+			  <option value="2016">2016</option>
+			  <option value="2017">2017</option>
+			  <option value="2018">2018</option>
+			  <option value="2019">2019</option>
+			</select>
+			<select name = "Monthe">
+			  <option value="01">01</option>
+			  <option value="02">02</option>
+			  <option value="03">03</option>
+			  <option value="04">04</option>
+			  <option value="05">05</option>
+			  <option value="06">06</option>
+			  <option value="07">07</option>
+			  <option value="08">08</option>
+			  <option value="09">09</option>
+			  <option value="10">10</option>
+			  <option value="11">11</option>
+			  <option value="12">12</option>
+			 </select>
+			
+
+			 <select name = "DDe">
+			 	<?php  
+					for ($x = 0; $x <= 9; $x++) {
+  						echo "<option value=0".$x. "> 0" .$x. "</option>";
+					}
+					for ($x = 10; $x <= 31; $x++) {
+  						echo "<option value=".$x. ">" .$x. "</option>";
+					}
+				?> 
+			 </select>
+
+			 <select name = "Tvale">
+			 	<option value="T">T</option>
+			 </select>
+
+
+			 <select name = "HHe">
+			 	<?php  
+					for ($x = 0; $x <= 9; $x++) {
+  						echo "<option value=0".$x. "> 0" .$x. "</option>";
+					}
+					for ($x = 10; $x <= 12; $x++) {
+  						echo "<option value=".$x. ">" .$x. "</option>";
+					}
+				?> 
+			 </select>
+
+			 <select name = "MMe">
+			 	<?php  
+					for ($x = 0; $x <= 9; $x++) {
+  						echo "<option value=0".$x. "> 0" .$x. "</option>";
+					}
+					for ($x = 10; $x <= 60; $x++) {
+  						echo "<option value=".$x. ">" .$x. "</option>";
+					}
+				?> 
+			 </select>
+
+			 <select name = "SSe">
+			 	<?php  
+					for ($x = 0; $x <= 9; $x++) {
+  						echo "<option value=0".$x. "> 0" .$x. "</option>";
+					}
+					for ($x = 10; $x <= 60; $x++) {
+  						echo "<option value=".$x. ">" .$x. "</option>";
+					}
+
+				?> 
+			 </select>
+
+			 <select name = "Zvale">
+			 	<option value="Z">T</option>
+			 </select>
+
+			 <!--  -->
+			 <!-- END OF END TIME! -->
+			 <!--  -->
+
+
+			 <?php 
+			 $varrStartTime = "".$_POST["YYYY"]. "-" .$_POST["Month"]. "-" .$_POST["DD"]. "" .$_POST["Tval"]. "" .$_POST["HH"]. ":" .$_POST["MM"]. ":" .$_POST["SS"]. "".$_POST["Zval"]."";
+			 ?>
+
+			 <?php 
+			 $varrEndTime = "".$_POST["YYYYe"]. "-" .$_POST["Monthe"]. "-" .$_POST["DDe"]. "" .$_POST["Tvale"]. "" .$_POST["HHe"]. ":" .$_POST["MMe"]. ":" .$_POST["SSe"]. "".$_POST["Zvale"]."";
+			 ?>
+
+
+
+			
 			<p>Location and Zip code</p>
 			<select name="location">
 				<?php echo $option; ?>
@@ -47,17 +223,22 @@
 			<select name = "groupID">
 				<?php echo $groupOption; ?>
 			</select>
+			
+
 			</br>
 			<input type="submit" name="createEvent" />
 		</form>
+
+
+
 		<?php
 			if(isset($_POST["createEvent"])){
-				if(!empty($_POST["title"]) && !empty($_POST["description"]) && !empty($_POST["startTime"])  && !empty($_POST["endTime"]) && !empty($_POST["location"]) && !empty($_POST["groupID"])){
+				if(!empty($_POST["title"]) && !empty($_POST["description"]) && !empty($_POST["location"]) && !empty($_POST["groupID"])){
 					//Assign variables to user inputted values
 					$eventTitle = stripslashes($_POST["title"]);
 					$eventDescription = stripslashes($_POST["description"]);
-					$eventStartTime = stripslashes($_POST["startTime"]);
-					$eventEndTime = stripslashes($_POST["endTime"]);
+					$eventStartTime = stripslashes($varrStartTime);
+					$eventEndTime = stripslashes($varrEndTime);
 					
 					$eventLocationZipcode = explode('@', $_POST["location"], 2);
 					
